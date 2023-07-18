0a8360d73db7f62bc7bbdb4e065d9b448194a94e
pwc-mw <michel.wu@pwc.com>
2023-07-18 14:17:41 +0200

    first commit

---
+++ ww-config.js
@@ -0,0 +1,145 @@
+export default {
+  editor: {
+    label: {
+      en: "Toggle",
+      fr: "Toggle",
+    },
+    icon: "toggle",
+  },
+  triggerEvents: [
+    {
+      name: "change",
+      label: { en: "On change" },
+      event: { value: "" },
+      default: true,
+    },
+    {
+      name: "initValueChange",
+      label: { en: "On init value change" },
+      event: { value: "" },
+    },
+  ],
+  properties: {
+    value: {
+      label: {
+        en: "Init value",
+      },
+      type: "OnOff",
+      section: "settings",
+      bindable: true,
+      /* wwEditor:start */
+      bindingValidation: {
+        type: "boolean",
+        tooltip: "A boolean that defines the initial value: `true | false`",
+      },
+      /* wwEditor:end */
+    },
+    required: {
+      label: "Required",
+      type: "OnOff",
+      section: "settings",
+      defaultValue: true,
+      bindable: true,
+      /* wwEditor:start */
+      bindingValidation: {
+        type: "boolean",
+        tooltip:
+          "A boolean that defines if the input is required: `true | false`",
+      },
+      /* wwEditor:end */
+    },
+    readonly: {
+      label: { en: "Read only", fr: "Lecture seule" },
+      type: "OnOff",
+      section: "settings",
+      bindable: true,
+      defaultValue: false,
+      hidden: (content, sidePanelContent, boundProps, wwProps) =>
+        !!(wwProps && wwProps.readonly !== undefined),
+    },
+    selectorSize: {
+      type: "Length",
+      label: {
+        en: "Selector size",
+        fr: "Taille du selecteur",
+      },
+      bindable: true,
+      options: {
+        unitChoices: [{ value: "%", label: "%", min: 10, max: 100 }],
+      },
+      defaultValue: "100%",
+      states: true,
+      classes: true,
+      responsive: true,
+    },
+    selectorColorOff: {
+      type: "Color",
+      label: {
+        en: "Selector color (off)",
+        fr: "Couleur du selecteur (off)",
+      },
+      bindable: true,
+      options: {
+        nullable: true,
+        gradient: true,
+      },
+      states: true,
+      classes: true,
+      responsive: true,
+    },
+    selectorColorOn: {
+      type: "Color",
+      label: {
+        en: "Selector color (on)",
+        fr: "Couleur du selecteur (on)",
+      },
+      bindable: true,
+      options: {
+        nullable: true,
+        gradient: true,
+      },
+      states: true,
+      classes: true,
+      responsive: true,
+    },
+    backgroundColorOff: {
+      type: "Color",
+      label: {
+        en: "Background color (off)",
+        fr: "Couleur de fond (off)",
+      },
+      bindable: true,
+      options: {
+        nullable: true,
+        gradient: true,
+      },
+      states: true,
+      classes: true,
+      responsive: true,
+    },
+    backgroundColorOn: {
+      type: "Color",
+      label: {
+        en: "Background color (on)",
+        fr: "Couleur de fond (on)",
+      },
+      bindable: true,
+      options: {
+        nullable: true,
+        gradient: true,
+      },
+      states: true,
+      classes: true,
+      responsive: true,
+    },
+    toggleState: {
+      type: "Button",
+      options: {
+        text: { en: "Toggle state", fr: "Toggle state" },
+        color: "blue",
+        action: "toggleValue",
+      },
+      editorOnly: true,
+    },
+  },
+};
